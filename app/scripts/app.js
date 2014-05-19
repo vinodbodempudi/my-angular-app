b6d2fa266a664c269f536d4a58e49cd4b432e167
vinodbodempudi <vinodbodempudi@gmail.com>
2014-05-18 17:56:25 -0700

    Signin validation
    Signin and register validation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,13 @@
 'use strict';
 
-angular.module('fatHomesApp', [
+var app = angular.module('fatHomesApp', [
   'ngCookies',
   'ngSanitize',
   'ngRoute',
   'ngResource'
-])
-  .config(function ($routeProvider) {
+]);
+  
+app.config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -28,3 +29,20 @@ angular.module('fatHomesApp', [
         redirectTo: '/'
       });
   });
+
+ app.directive('match', function () {
+        return {
+            require: 'ngModel',
+            restrict: 'A',
+            scope: {
+                match: '='
+            },
+            link: function(scope, elem, attrs, ctrl) {
+                scope.$watch(function() {
+                    return (ctrl.$pristine && angular.isUndefined(ctrl.$modelValue)) || scope.match === ctrl.$modelValue;
+                }, function(currentValue) {
+                    ctrl.$setValidity('match', currentValue);
+                });
+            }
+        };
+    });
